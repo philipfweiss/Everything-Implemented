ef8a1cd380723a3d7c0f7302032d8fc960e49878
philipfweiss <weissp68@stanford.edu>
2016-10-18 00:33:23 -0700

    Basic LinkedList Class

--- LinkedList.rb
+++ LinkedList.rb
@@ -1,3 +1,4 @@
+## Philip Feldman Weiss - 10/18/2016
 ## This is an implementation of a very simple linked list in Ruby.
 
 class ListNode
@@ -12,17 +13,20 @@ class ListNode
 end
 
 class LinkedList
-  def initialize
-    @head = nil
+
+  ## Initializes LinkedList Class
+  def initialize(val)
+    @head = ListNode.new(val)
     @last = @head
     @size = 0
   end
 
+  ## Returns the size of the LinkedList
   def size
     @size
   end
 
-
+  ## Adds a new node to the end of the LinkedList
   def push_node(val)
     new_node = ListNode.new(val)
     @last.next = new_node
@@ -30,6 +34,21 @@ class LinkedList
     @size+=1
   end
 
+
+  ## Get the nth node from the LinkedList
+  def get_node(int)
+    cur_node = @head
+    int.times do
+      cur_node = cur_node.next
+      if cur_node.nil?
+        return nil
+      end
+    end
+    cur_node
+  end
+
+
+  ## Print all the nodes in the LinkedList
   def print_nodes
     cur_node = @head
     until cur_node.nil?
@@ -40,4 +59,11 @@ class LinkedList
 end
 
 ## Let's test the class down here.
+ll = LinkedList.new("Kop")
+10.times do |x|
+  ll.push_node(x)
+end
 
+puts "Fifth node: #{ll.get_node(5)}"
+puts "Size: #{ll.size}"
+ll.print_nodes
